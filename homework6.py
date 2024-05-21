713ea74a6b88f9b8531d6701c41c923d1a7ca75f
Sofiakhis <sofiakhismatova@mail.ru>
2024-05-21 18:35:49 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,18 @@
+my_list = ['яблоко', 'апельсин', 'груша', 'банан', 'клубника', 'дыня']
+print(my_list[0])
+# print(my_list[5])
+# print(my_list[3:5])
+#my_list[3] = 'strawberry'
+#print(my_list)
+my_dict = {'солнце': 'sun', 'луна': 'moon',}
+print(my_dict['солнце'])
+my_dict['облако'] = 'cloud'
+print(my_dict)
+
+
+
+
+
+
+
+
